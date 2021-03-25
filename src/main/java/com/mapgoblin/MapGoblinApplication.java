18418074a762e2c579f658bd6e3a3d0ee48a9148
SimHongSub <ghdtjq2038@gmail.com>
2021-03-25 19:32:53 +0900

    create mapgoblin backend project

---
+++ src/main/java/com/mapgoblin/MapGoblinApplication.java
@@ -0,0 +1,13 @@
+package com.mapgoblin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapGoblinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapGoblinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mapgoblin/MapGoblinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapgoblin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapGoblinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
