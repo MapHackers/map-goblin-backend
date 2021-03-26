4c028165ae94e1c804d447b6e21d99f340d7253c
SimHongSub <ghdtjq2038@gmail.com>
2021-03-26 22:09:18 +0900

    [domain] add Member entity

---
+++ src/main/java/com/mapgoblin/domain/Member.java
@@ -0,0 +1,35 @@
+package com.mapgoblin.domain;
+
+import com.mapgoblin.domain.base.BaseEntity;
+import com.mapgoblin.domain.base.MemberRole;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class Member extends BaseEntity{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String userId;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private int reward;
+
+    @Enumerated(EnumType.STRING)
+    private MemberRole role;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberRepository> repositories = new ArrayList<>();
+}
