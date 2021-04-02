91b9766f2f3863bfa3b59825c6808be6d20e896f
SimHongSub <ghdtjq2038@gmail.com>
2021-04-02 16:42:04 +0900

    [space] add repository create api

--- src/main/java/com/mapgoblin/api/controller/SpaceApi.java
+++ src/main/java/com/mapgoblin/api/controller/SpaceApi.java
@@ -1,25 +1,36 @@
 package com.mapgoblin.api.controller;
 
 import com.mapgoblin.api.dto.ApiResult;
+import com.mapgoblin.api.dto.space.CreateSpaceRequest;
+import com.mapgoblin.api.dto.space.CreateSpaceResponse;
 import com.mapgoblin.api.dto.space.SpaceDto;
+import com.mapgoblin.domain.Map;
+import com.mapgoblin.domain.Member;
+import com.mapgoblin.domain.MemberSpace;
 import com.mapgoblin.domain.Space;
 import com.mapgoblin.service.SpaceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/repositories")
 @RequiredArgsConstructor
 public class SpaceApi {
 
     private final SpaceService spaceService;
 
-    @GetMapping("/repositories")
-    public ResponseEntity<?> repositories() {
+    /**
+     * Get all repositories
+     *
+     * @return
+     */
+    @GetMapping
+    public ResponseEntity<?> get() {
 
         List<Space> spaceList = spaceService.findAll();
 
@@ -28,5 +39,20 @@ public class SpaceApi {
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(new ApiResult(collect));
+    }
+
+    /**
+     * Create repository
+     *
+     * @param request
+     * @param member
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody CreateSpaceRequest request, @AuthenticationPrincipal Member member) {
+
+        CreateSpaceResponse response = spaceService.create(member.getId(), request);
+
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/mapgoblin/service/SpaceService.java
+++ src/main/java/com/mapgoblin/service/SpaceService.java
@@ -1,19 +1,72 @@
 package com.mapgoblin.service;
 
+import com.mapgoblin.api.dto.space.CreateSpaceRequest;
+import com.mapgoblin.api.dto.space.CreateSpaceResponse;
+import com.mapgoblin.domain.Map;
+import com.mapgoblin.domain.Member;
+import com.mapgoblin.domain.MemberSpace;
 import com.mapgoblin.domain.Space;
+import com.mapgoblin.repository.MapRepository;
+import com.mapgoblin.repository.MemberRepository;
+import com.mapgoblin.repository.MemberSpaceRepository;
 import com.mapgoblin.repository.SpaceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class SpaceService {
 
+    private final MemberRepository memberRepository;
     private final SpaceRepository spaceRepository;
+    private final MapRepository mapRepository;
+    private final MemberSpaceRepository memberSpaceRepository;
 
+    /**
+     * Find all repositories
+     *
+     * @return
+     */
     public List<Space> findAll() {
         return spaceRepository.findAll();
+    }
+
+    /**
+     * Create Space
+     *
+     * @param memberId
+     * @param request
+     * @return
+     */
+    @Transactional
+    public CreateSpaceResponse create(Long memberId, CreateSpaceRequest request) {
+
+        Member member = memberRepository.findById(memberId).orElse(null);
+        Map map = Map.createMap();
+
+        mapRepository.save(map);
+
+        Space space = Space.createSpace(request.getName(), request.getThumbnail(), request.getDescription(), map);
+
+        MemberSpace memberSpace = MemberSpace.createMemberSpace(space);
+
+        member.addMemberSpace(memberSpace);
+
+        memberSpaceRepository.save(memberSpace);
+
+        spaceRepository.save(space);
+
+        return new CreateSpaceResponse(
+                space.getId(),
+                space.getMap().getId(),
+                space.getName(),
+                space.getThumbnail(),
+                space.getDescription(),
+                space.getLikeCount(),
+                space.getDislikeCount());
     }
 }
