14a123dfb01b93abed24a074bf47fb7e5194294f
SimHongSub <ghdtjq2038@gmail.com>
2021-05-06 23:56:53 +0900

    [space] modify create space dto

--- src/main/java/com/mapgoblin/api/dto/space/CreateSpaceRequest.java
+++ src/main/java/com/mapgoblin/api/dto/space/CreateSpaceRequest.java
@@ -3,10 +3,13 @@ package com.mapgoblin.api.dto.space;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 public class CreateSpaceRequest {
     private String name;
     private String thumbnail;
     private String description;
+    private List<String> categories;
 }
