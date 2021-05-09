741c6f8bf6d543e0fac63b88905344ec7c805c9b
SimHongSub <ghdtjq2038@gmail.com>
2021-05-09 13:39:34 +0900

    [space] create SpaceCategoryRepository

---
+++ src/main/java/com/mapgoblin/repository/SpaceCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.mapgoblin.repository;
+
+import com.mapgoblin.domain.SpaceCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpaceCategoryRepository extends JpaRepository<SpaceCategory, Long> {
+}
