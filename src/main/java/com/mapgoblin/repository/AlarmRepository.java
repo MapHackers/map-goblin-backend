fe8123d02dc083c1755c997757fb60aa4be96e81
SimHongSub <ghdtjq2038@gmail.com>
2021-05-19 22:33:20 +0900

    [space] modify repository delete

--- src/main/java/com/mapgoblin/repository/AlarmRepository.java
+++ src/main/java/com/mapgoblin/repository/AlarmRepository.java
@@ -2,6 +2,7 @@ package com.mapgoblin.repository;
 
 import com.mapgoblin.domain.Alarm;
 import com.mapgoblin.domain.Member;
+import com.mapgoblin.domain.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 
     Optional<List<Alarm>> findByDstMemberOrderByCreatedDateDesc(Member member);
+
+    Optional<List<Alarm>> findByDstSpace(Space space);
 }
