f19c6e85eac0faec981750d75977b8b0b822019c
SimHongSub <ghdtjq2038@gmail.com>
2021-05-28 23:20:36 +0900

    [space] issue create api

--- src/main/java/com/mapgoblin/api/controller/IssueApi.java
+++ src/main/java/com/mapgoblin/api/controller/IssueApi.java
@@ -1,14 +1,29 @@
 package com.mapgoblin.api.controller;
 
+import com.mapgoblin.domain.Member;
 import com.mapgoblin.service.IssueService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
 public class IssueApi {
 
     private final IssueService issueService;
+
+    @PostMapping("/issues")
+    public ResponseEntity<?> create(@RequestBody HashMap<String, String> request){
+        
+
+        return ResponseEntity.ok("");
+    }
 
 
 }
