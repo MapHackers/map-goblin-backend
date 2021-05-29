7793bd8f7cad9aa5766b96e3a2685a753cd92abc
SimHongSub <ghdtjq2038@gmail.com>
2021-05-29 11:26:55 +0900

    [space] add issue create api, get list for one repository api

--- src/main/java/com/mapgoblin/repository/IssueRepository.java
+++ src/main/java/com/mapgoblin/repository/IssueRepository.java
@@ -1,7 +1,14 @@
 package com.mapgoblin.repository;
 
 import com.mapgoblin.domain.Issue;
+import com.mapgoblin.domain.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IssueRepository extends JpaRepository<Issue, Long> {
+
+    Optional<List<Issue>> findBySpace(Space space);
+
 }
--- src/main/java/com/mapgoblin/service/IssueService.java
+++ src/main/java/com/mapgoblin/service/IssueService.java
@@ -1,4 +1,43 @@
 package com.mapgoblin.service;
 
+import com.mapgoblin.api.dto.issue.CreateIssueResponse;
+import com.mapgoblin.api.dto.issue.GetIssueResponse;
+import com.mapgoblin.domain.Issue;
+import com.mapgoblin.domain.Space;
+import com.mapgoblin.repository.IssueRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class IssueService {
+
+    private final IssueRepository issueRepository;
+
+    @Transactional
+    public CreateIssueResponse save(Issue issue) {
+        issueRepository.save(issue);
+
+        return new CreateIssueResponse(issue.getTitle(), issue.getContent(),
+                issue.getStatus(), issue.getCreatedDate(), issue.getCreatedBy());
+    }
+
+    public List<GetIssueResponse> findBySpace(Space space){
+        List<GetIssueResponse> result = null;
+        List<Issue> issueList = issueRepository.findBySpace(space).orElse(null);
+
+        if (issueList != null){
+            result = issueList.stream().map(issue -> {
+                return new GetIssueResponse(issue.getTitle(), issue.getContent(),
+                        issue.getStatus(), issue.getCreatedDate(), issue.getCreatedBy());
+            }).collect(Collectors.toList());
+        }
+
+        return result;
+    }
 }
