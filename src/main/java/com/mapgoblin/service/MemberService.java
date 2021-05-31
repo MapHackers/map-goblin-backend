4ffc8277711ecafc8ba8c51f9aeb32b4b18279de
DOIL KIM <doili0552@gmail.com>
2021-05-31 13:36:58 +0900

    [search] add searching with description
    add searching with description

--- src/main/java/com/mapgoblin/service/MemberService.java
+++ src/main/java/com/mapgoblin/service/MemberService.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -116,6 +117,7 @@ public class MemberService implements UserDetailsService {
     }
 
     public List<Member> search(String keyword){
+
         return memberRepository.findByNameContaining(keyword).orElse(null);
     }
 }
