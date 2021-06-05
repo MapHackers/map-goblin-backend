d271c259241b0da608435d1615c224000388fc07
SimHongSub <ghdtjq2038@gmail.com>
2021-06-05 13:19:51 +0900

    [space] add already request data check logic

--- src/main/java/com/mapgoblin/repository/RequestDataRepository.java
+++ src/main/java/com/mapgoblin/repository/RequestDataRepository.java
@@ -3,5 +3,9 @@ package com.mapgoblin.repository;
 import com.mapgoblin.domain.RequestData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RequestDataRepository extends JpaRepository<RequestData, Long> {
+
+    Optional<RequestData> findByMapDataIdAndLayerId(Long dataId, Long layerId);
 }
