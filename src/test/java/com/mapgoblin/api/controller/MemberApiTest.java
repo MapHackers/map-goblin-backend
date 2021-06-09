e47ab4660858794354b26ab3e0aa1b3b230dab22
SimHongSub <ghdtjq2038@gmail.com>
2021-06-09 18:38:14 +0900

    [space] modify test setting

--- src/test/java/com/mapgoblin/api/controller/MemberApiTest.java
+++ src/test/java/com/mapgoblin/api/controller/MemberApiTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
-@Rollback(value = true)
+@Rollback(value = false)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MemberApiTest {
 
