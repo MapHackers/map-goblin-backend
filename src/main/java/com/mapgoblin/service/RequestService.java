0d540982257eb5fc4f9dc68c620b74b25a12fee1
SimHongSub <ghdtjq2038@gmail.com>
2021-09-03 21:30:23 +0900

    space api refactoring

--- src/main/java/com/mapgoblin/service/RequestService.java
+++ src/main/java/com/mapgoblin/service/RequestService.java
@@ -42,26 +42,9 @@ public class RequestService {
     }
 
     @Transactional
-    public Long save(Request request, /*HashMap<String, List<HashMap<String, String>>> data*/ RequestDataDto data){
+    public Long save(Request request, RequestDataDto data){
 
         requestRepository.save(request);
-
-//        if(data.containsKey("added")){
-//            List<HashMap<String, String>> added = data.get("added");
-//
-//            for (HashMap<String, String> addedData : added) {
-//                RequestData requestData = RequestData.create(Long.parseLong(addedData.get("id")),
-//                        Long.parseLong(addedData.get("layerId")),
-//                        addedData.get("name"),
-//                        LocalDateTime.parse(addedData.get("createdDate")),
-//                        addedData.get("geometry"),
-//                        RequestAction.INSERT);
-//
-//                request.addRequestData(requestData);
-//
-//                requestDataRepository.save(requestData);
-//            }
-//        }
 
         List<HashMap<String, String>> added = data.getAdded();
 
@@ -80,23 +63,6 @@ public class RequestService {
             }
         }
 
-//        if(data.containsKey("modified")){
-//            List<HashMap<String, String>> modified = data.get("modified");
-//
-//            for (HashMap<String, String> modifiedData : modified) {
-//                RequestData requestData = RequestData.create(Long.parseLong(modifiedData.get("id")),
-//                        Long.parseLong(modifiedData.get("layerId")),
-//                        modifiedData.get("name"),
-//                        LocalDateTime.parse(modifiedData.get("createdDate")),
-//                        modifiedData.get("geometry"),
-//                        RequestAction.UPDATE);
-//
-//                request.addRequestData(requestData);
-//
-//                requestDataRepository.save(requestData);
-//            }
-//        }
-
         List<HashMap<String, String>> modified = data.getModified();
 
         if(modified != null) {
@@ -114,24 +80,6 @@ public class RequestService {
             }
         }
 
-
-//        if(data.containsKey("delete")){
-//            List<HashMap<String, String>> delete = data.get("delete");
-//
-//            for (HashMap<String, String> deleteData : delete) {
-//                RequestData requestData = RequestData.create(Long.parseLong(deleteData.get("id")),
-//                        Long.parseLong(deleteData.get("layerId")),
-//                        deleteData.get("name"),
-//                        LocalDateTime.parse(deleteData.get("createdDate")),
-//                        deleteData.get("geometry"),
-//                        RequestAction.DELETE);
-//
-//                request.addRequestData(requestData);
-//
-//                requestDataRepository.save(requestData);
-//            }
-//        }
-
         List<HashMap<String, String>> delete = data.getDelete();
 
         if(delete != null) {
@@ -148,23 +96,6 @@ public class RequestService {
                 requestDataRepository.save(requestData);
             }
         }
-
-//        if(data.containsKey("layer")){
-//            List<HashMap<String, String>> layer = data.get("layer");
-//
-//            for (HashMap<String, String> layerData : layer) {
-//                RequestData requestData = RequestData.create(null,
-//                        Long.parseLong(layerData.get("layerId")),
-//                        layerData.get("name"),
-//                        LocalDateTime.parse(layerData.get("createdDate")),
-//                        layerData.get("geometry"),
-//                        RequestAction.INSERT);
-//
-//                request.addRequestData(requestData);
-//
-//                requestDataRepository.save(requestData);
-//            }
-//        }
 
         List<HashMap<String, String>> layer = data.getLayer();
 
